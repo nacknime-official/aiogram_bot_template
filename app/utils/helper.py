78f3fd9938d847fbc3246abe83cd0b3eb72922fe
nacknime-official <lenovo.maks@gmail.com>
2020-10-09 12:31:36 +0300

    added basic helper functions

---
+++ app/utils/helper.py
@@ -0,0 +1,18 @@
+from typing import List, Optional, Union
+
+from aiogram.utils.markdown import link
+
+
+def get_user_link(user_id: Union[str, int], text: Optional[str] = None) -> str:
+    """
+    Get markdown link on a user
+    Used in the admin's block commands
+
+    :param user_id:   user's id
+    :param text:      text of the link
+
+    :returns:         link
+    """
+    if not text:
+        text = user_id
+    return link(str(text), f"tg://user?id={user_id}")
