da595bec337a3d44829135cccbb78b69055d57a6
nacknime-official <lenovo.maks@gmail.com>
2020-10-09 10:33:36 +0300

    added misc.py

---
+++ app/misc.py
@@ -0,0 +1,15 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from aiogram.utils.executor import Executor
+
+from app import config
+
+bot = Bot(config.TELEGRAM_TOKEN)
+storage = RedisStorage2(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_FSM_DB,
+    prefix=config.REDIS_FSM_PREFIX,
+)
+dp = Dispatcher(bot, storage=storage)
+runner = Executor(dp)
